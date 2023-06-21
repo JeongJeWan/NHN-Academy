0e4d87be30273690199b71ddb40aa74692cf338a
JeongJeWan <jewan8821@naver.com>
2023-06-21 10:25:49 +0900

    JAVA-TIP-Thread

--- JAVA-TIP-Thread/BlockBrakeGame/src/main/java/org/nhnacademy/game/test/Bar.java
+++
@@ -1,8 +0,0 @@
-package org.nhnacademy.game.test;
-
-public class Bar {
-    int x = MyFrame.CANVAS_WIDTH/2 - MyFrame.BAR_WIDTH/2;
-    int y = MyFrame.CANVAS_HEIGHT - 100;
-    int width = MyFrame.BAR_WIDTH;
-    int height = MyFrame.BAR_HEIGHT;
-}
--- JAVA-TIP-Thread/BlockBrakeGame/src/test/java/org/nhnacademy/game/test/BlockTest.java
+++
@@ -1,7 +0,0 @@
-package org.nhnacademy.game.test;
-
-class BlockTest {
-    public static void main(String[] args) {
-        new MyFrame("Block Brake Game");
-    }
-}
